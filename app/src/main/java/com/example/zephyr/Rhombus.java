ef9c60634a552a2f7149b65fb99a8dbee17656f4
Jason Hu <garbhj6@gmail.com>
2026-01-27 19:05:41 -0700

    feat: add color to Rhombus

--- app/src/main/java/com/example/zephyr/Rhombus.java
+++ app/src/main/java/com/example/zephyr/Rhombus.java
@@ -1,4 +1,9 @@
 package com.example.zephyr;
 
-public class Rhombus {
+public class Rhombus extends Shape {
+    private String color = "blue";
+
+    public Rhombus(int x, int y) {
+        super(x, y);
+    }
 }
