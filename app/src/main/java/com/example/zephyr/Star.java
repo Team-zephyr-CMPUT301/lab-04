73fe9f3e460f018765051de402f010ff03ed96a7
Yani <Yanixiv.dev@gmail.com>
2026-01-27 18:31:50 -0700

    star shape

---
+++ app/src/main/java/com/example/zephyr/Star.java
@@ -0,0 +1,10 @@
+package com.example.zephyr;
+
+public class Star extends Shape {
+    Integer x;
+    Integer y;
+    public Star(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
